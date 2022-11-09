a1f245100449bbde84ca6b9d800adfbfb9544b21
chaknab1 <35934097+chaknab1@users.noreply.github.com>
2022-11-09 18:40:04 +0530

    Update streamlit_app.py
    add a new button to load fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,37 +42,35 @@ try:
     streamlit.error("Please select fruit to get information")
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
-    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    #streamlit.dataframe(fruityvice_normalized)
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
 streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-#streamlit.text(fruityvice_response.json()) # writes the data on screen
-
-# normalized the json 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the dataframe
-#streamlit.dataframe(fruityvice_normalized)
-
 # don't run anything from here for now
-streamlit.stop()
+#streamlit.stop()
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
-
-my_data_row = my_cur.fetchall()
 streamlit.text("The Fruit Load List contains:")
-streamlit.dataframe(my_data_row)
+# Snowflake-related function
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+  
+# add a button to load fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
+
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("select * from fruit_load_list")
+#my_data_row = my_cur.fetchall()
+#streamlit.text("The Fruit Load List contains:")
+#streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', add_my_fruit)
