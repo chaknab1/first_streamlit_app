973919c052dc5626120e4c7039470f8ec37521fb
chaknab1 <35934097+chaknab1@users.noreply.github.com>
2022-11-09 20:58:24 +0530

    Update streamlit_app.py
    close connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,6 +62,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_row = get_fruit_load_list()
+  my_cnx.close() # added to close the connection
   streamlit.dataframe(my_data_row)
 
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
